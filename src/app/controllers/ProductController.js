12c88803c82330f16d9a1ae8c9bf7397dac81246
Adriano Martins <adrianomartins002@gmail.com>
2020-05-23 12:04:53 -0300

    feat(productservice, testes): criar funcao no product service para o retorno dos produtos e testes para a msema

--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -2,7 +2,7 @@ import ProductService from "../services/ProductService"
 
 class ProductController {
   index = async (req, res) => {
-    return res.json(await ProductService.index());
+    return res.json(await ProductService.returnProducts());
   }
 }
 
